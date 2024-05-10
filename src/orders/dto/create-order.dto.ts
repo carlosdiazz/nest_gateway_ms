89f57f412b9a4026878661a141343250179398cb
Carlos DIaz <c.diazadriann@gmail.com>
2024-05-10 16:57:05 -0400

    gateway con ORders

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,28 +1,31 @@
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
-import { OrderStatus, OrderStatusList } from '../enum/orders.enum';
+import { Type } from 'class-transformer';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
+  //@IsNumber()
+  //@IsPositive()
+  //totalAmount: number;
+  //
+  //@IsNumber()
+  //@IsPositive()
+  //totalItems: number;
+  //
+  //@IsOptional()
+  //@IsEnum(OrderStatusList, {
+  //  message: `Possible status values are ${OrderStatusList}`,
+  //})
+  //status: OrderStatus = OrderStatus.PENDING;
+  //
+  //@IsOptional()
+  //@IsBoolean()
+  //paid: boolean = false;
 
-  @IsNumber()
-  @IsPositive()
-  totalItems: number;
-
-  @IsOptional()
-  @IsEnum(OrderStatusList, {
-    message: `Possible status values are ${OrderStatusList}`,
-  })
-  status: OrderStatus = OrderStatus.PENDING;
-
-  @IsOptional()
-  @IsBoolean()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  //Validar internamente los elementos que vienen
+  @ValidateNested({ each: true })
+  //Cada Itereacion la va a transformar a mi DTO
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
